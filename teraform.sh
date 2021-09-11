f089992806097834deed0a2103b4e1dc9a5f2219
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-09-11 18:35:29 +0300

    Update teraform.sh

--- teraform.sh
+++ teraform.sh
@@ -6,3 +6,11 @@ sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.rel
 sudo  apt update
 sudo apt install terraform
 sudo terraform -v
+
+####################AWS CLI#############################
+sudo apt update
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+sudo apt install unzip -y 
+sudo unzip awscliv2.zip
+sudo ./aws/install
+aws --version
