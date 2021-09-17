ae023b0513b5e4bed5dcd17f9d2dedeeb2ab5330
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-09-17 21:23:48 +0300

    Update teraformTestForJenkins.tf

--- teraformTestForJenkins.tf
+++ teraformTestForJenkins.tf
@@ -1,18 +1,20 @@
 provider "aws" {
     region      = "eu-north-1"
-  
+     access_key = ""
+     secret_key = ""
+    
 }
 
-resource "aws_instance" "test_jenkins_terraform" {
-
-    count                  = 1
-    ami                    = "ami-0ed17ff3d78e74700"
-    instance_type          = "t3.micro"
-    vpc_security_group_ids = ["sg-0446a23868282b71f"]
-    key_name               = "ubuntu"
-    tags = {
-        Name    = "Test Jenkins_terrafrom"
-        Owner   = "Nazar"
-        Project = "Jenkins_Terrafrorm"
-    }
+resource "aws_instance" "Jenkins_for_Terraform" {
+  count                  = 1
+  ami                    = "ami-0ed17ff3d78e74700"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0446a23868282b71f"]
+  key_name               = "devops_course_work"
+  
+  tags = {
+    Name    = "Created using terraform"
+    Owner   = "Nazar"
+    Project = "Test_how_to_start_terraform_by_Jenkins"
+  }
 }
