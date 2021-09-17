4064cc9a1075bf5ee89b13b7ea7a3722441027c8
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-09-17 21:37:49 +0300

    Update teraformTestForJenkins.tf

--- teraformTestForJenkins.tf
+++ teraformTestForJenkins.tf
@@ -1,15 +1,27 @@
-provider "aws" {
-    region      = "eu-north-1"
-     access_key = ""
-     secret_key = ""
-    
+resource "aws_security_group" "web_server" {
+  
+  name_prefix = "web"
+  dynamic "ingress" {
+    for_each = ["80", "433", "8080", "22"]
+    content {
+      from_port = ingress.value
+      protocol = "tcp"
+      to_port = ingress.value
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
 resource "aws_instance" "Jenkins_for_Terraform" {
   count                  = 1
   ami                    = "ami-0ed17ff3d78e74700"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0446a23868282b71f"]
+  vpc_security_group_ids = [aws_security_group.web_server.id]
   key_name               = "devops_course_work"
   
   tags = {
