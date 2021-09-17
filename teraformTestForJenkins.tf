57e4903646cc690fd9b1b2302a64049f52db1c70
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-09-17 21:38:50 +0300

    Update teraformTestForJenkins.tf

--- teraformTestForJenkins.tf
+++ teraformTestForJenkins.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region      = "eu-north-1"
-     access_key = "AKIAVYLC26J2GBLUCCFB"
-     secret_key = "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"
+     access_key = ""
+     secret_key = ""
     
 }
 resource "aws_security_group" "web_server" {
