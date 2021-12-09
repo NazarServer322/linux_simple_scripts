5e49229394d768a276e01c0100e97d566942daca
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-12-09 20:27:27 +0200

    Create ip.sh

---
+++ ip.sh
@@ -0,0 +1,6 @@
+yum -y update
+yum -y install httpd
+PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo “Web Server has $PRIVATE_IP “ > /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
